6e0e480aca254e5575ac3d4d6c5d2311847162e8
t0yohei <inuneko690121@gmail.com>
2019-04-16 01:45:15 +0900

    babel

---
+++ main_compiled.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var _submodule = require('./submodule_compiled.js');
+
+var _submodule2 = _interopRequireDefault(_submodule);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+console.log((0, _submodule2.default)(2, 3));
---
+++ submodule_compiled.js
@@ -0,0 +1,9 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = plus;
+function plus(a, b) {
+  return a + b;
+}
